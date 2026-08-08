d280189a85d6bc4e5f419b30e793c7f0d9607521
fazkn <fazakamila5@gmail.com>
2026-08-08 15:11:00 +0700

    fix: memperbaiki tampilan grafik sebaran kelompok usia dan sebaran pendidikan

--- app/Filament/Widgets/AgeGroupChart.php
+++ app/Filament/Widgets/AgeGroupChart.php
@@ -11,7 +11,7 @@ class AgeGroupChart extends ChartWidget
 
     protected static ?int $sort = 2;
 
-    protected int | string | array $columnSpan = 1;
+    protected int | string | array $columnSpan = 'full';
 
     protected function getData(): array
     {
--- app/Filament/Widgets/EducationChart.php
+++ app/Filament/Widgets/EducationChart.php
@@ -11,7 +11,7 @@ class EducationChart extends ChartWidget
 
     protected static ?int $sort = 3;
 
-    protected int | string | array $columnSpan = 1;
+    protected int | string | array $columnSpan = 'full';
 
     protected function getData(): array
     {
